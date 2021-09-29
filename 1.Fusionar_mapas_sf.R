cab1ff877fab5c215568ae049534369598dd131a
J.F. Monteagudo <75076856+jfmont@users.noreply.github.com>
2021-09-29 14:50:05 +0200

    Add files via upload
    Breve descripción de como fusionar mapas con sf.

---
+++ 1.Fusionar_mapas_sf.R
@@ -0,0 +1,55 @@
+#### Paquetes ####
+
+pacman::p_load(sf, mapSpain, tidyverse)
+
+##### Ojo! Trabajo directamente con sf, si tenemos shp habria que importarlos
+#y pasarlos a sf#####
+
+#Mapa de españa
+
+sf_spain <- esp_get_ccaa() %>% 
+  
+  st_transform(crs = 4326)   #Transformamos de ETRS89 a WGS84
+  
+  
+#Cogemos Murcia, clm y comvalenciana
+  
+
+murcia <- sf_spain %>% 
+  
+          filter(ine.ccaa.name == "Murcia, Región de")
+
+
+clm <- sf_spain %>% 
+  
+          filter(ine.ccaa.name == "Castilla - La Mancha")
+
+cv <- sf_spain %>% 
+  
+          filter(ine.ccaa.name == "Comunitat Valenciana")
+
+
+# Ya las tenemos, primero comprobaríamos que todas tienen el mismo crs
+
+st_crs(murcia)
+
+st_crs(clm)
+
+st_crs(cv)
+
+
+# Todo el mapa junto (separado)
+
+juntos <- rbind(murcia, clm, cv)
+
+plot(juntos)
+
+# Todas las areas juntas
+
+prueba <- st_union(murcia, clm) %>% 
+          
+          st_union(cv)  
+
+plot(prueba)
+
+
